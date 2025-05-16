863d3ef390101d8e43e7a8554a25fa427b84f9dd
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-17 02:18:18 +0530

    io-operations

--- Strivers A2Z DSA Course/s1-learn the basics/lec-1/2-input-value.cpp
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/2-input-value.cpp
@@ -10,10 +10,17 @@ int main(){
     cin >> x; // Taking input from the user and storing it in the variable x
     cout << "value of x is: " << x << endl; // Outputting the value of x to the console
 
+    int y, z;
+    cout << "Enter two numbers: ";
+    cin >> y >> z; // Taking input for two variables y and z
+    cout << "value of y is : " << y << "and value of z is : " << z << endl; // Outputting the values of y and z
+
     return 0;
 }
 
-// PS D:\code\Leet-Code>  & 'c:\Users\allen\.vscode\extensions\ms-vscode.cpptools-1.25.3-win32-x64\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-jppnvrjd.s5t' '--stdout=Microsoft-MIEngine-Out-0ohksywx.bhi' '--stderr=Microsoft-MIEngine-Error-upuhjh55.rjh' '--pid=Microsoft-MIEngine-Pid-a0b0drew.zpa' '--dbgExe=C:\ProgramData\mingw64\mingw64\bin\gdb.exe' '--interpreter=mi' 
-// Enter a number: 
-// 10
-// value of x is: 10
+// Enter a number: 12
+// value of x is: 12
+// Enter two numbers: 10
+// 25
+// value of y is : 10and value of z is : 25
+// PS D:\code\Leet-Code> 
