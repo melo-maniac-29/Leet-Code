ccddade948ac9c8a0450b3365acf8995b8512292
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-17 02:40:52 +0530

    string working

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/6-stringissue.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    string s1, s2;
+
+    cout << "enter two strings: " << endl; // Prompting the user to enter two strings
+    
+    cin >> s1 >> s2; // Taking input from the user and storing it in the variables s1 and s2
+    // Outputting the values of s1 and s2 to the console
+    cout << "s1 is :" << s1 << " s2 is :" << s2 << endl;
+
+    return 0; 
+
+}
+
+// enter two strings: 
+// myself allen
+// s1 is :myself s2 is :allen
+// PS D:\code\Leet-Code> 
