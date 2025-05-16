683f58a6caf1e224eb8b40b439fa8042f3b83ff2
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-17 02:03:00 +0530

    namespace-std working

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/namespacestd.cpp
@@ -0,0 +1,23 @@
+#include<iostream> // Including the iostream library for input and output operations
+
+using namespace std; // Using the standard namespace to avoid prefixing std:: before standard functions and objects
+
+int main(){
+
+    cout << "Hello World-"; // Outputting "Hello World" to the console
+
+    cout << "using new line" << "\n"; // Outputting "using new line" followed by a newline character
+
+    cout << "new line" << endl; //endl similar to "\n" but also flushes the output buffer
+
+    cout <<"new line eg" << endl; // Outputting "new line eg" 
+
+    return 0;
+}
+
+// [Running] cd "d:\code\Leet-Code\Strivers A2Z DSA Course\s1-learn the basics\lec-1\" && g++ namespacestd.cpp -o namespacestd && "d:\code\Leet-Code\Strivers A2Z DSA Course\s1-learn the basics\lec-1\"namespacestd
+// Hello World-using new line
+// new line
+// new line eg
+
+// [Done] exited with code=0 in 0.507 seconds
