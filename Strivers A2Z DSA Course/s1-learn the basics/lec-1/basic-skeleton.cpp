35d4dc9b4201f09caf8554c20a60178c8ac33e60
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-17 01:59:02 +0530

    basic skeleton output operation

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/basic-skeleton.cpp
@@ -0,0 +1,22 @@
+#include<iostream> // Including the iostream library for input and output operations
+
+int main(){
+
+    std::cout << "Hello World-"; // Outputting "Hello World" to the console
+
+    std::cout << "using new line" << "\n"; // Outputting "using new line" followed by a newline character
+
+    std::cout << "new line" << std::endl; //endl similar to "\n" but also flushes the output buffer
+
+    std::cout <<"new line eg" << std::endl; // Outputting "new line eg" 
+
+    return 0;
+}
+
+
+// "d:\code\Leet-Code\Strivers A2Z DSA Course\s1-learn the basics\lec-1\"basic-skeleton
+// Hello World-using new line
+// new line
+// new line eg
+
+// [Done] exited with code=0 in 0.442 seconds
