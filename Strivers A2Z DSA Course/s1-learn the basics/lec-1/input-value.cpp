f8033d54a3bd73fcdaac1929194ef17e8c232752
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-17 02:11:46 +0530

    input-output operations

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/input-value.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int x;
+
+    cout << "Enter a number: ";
+    cin >> x; // Taking input from the user and storing it in the variable x
+    cout << "value of x is: " << x << endl; // Outputting the value of x to the console
+
+    return 0;
+}
+
+// PS D:\code\Leet-Code>  & 'c:\Users\allen\.vscode\extensions\ms-vscode.cpptools-1.25.3-win32-x64\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-jppnvrjd.s5t' '--stdout=Microsoft-MIEngine-Out-0ohksywx.bhi' '--stderr=Microsoft-MIEngine-Error-upuhjh55.rjh' '--pid=Microsoft-MIEngine-Pid-a0b0drew.zpa' '--dbgExe=C:\ProgramData\mingw64\mingw64\bin\gdb.exe' '--interpreter=mi' 
+// Enter a number: 
+// 10
+// value of x is: 10
