198090a816a6f8aa56093feeb42b4f4233fcd792
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-17 13:24:19 +0530

    string-getline

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/7-string(getline).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str;
+
+    cout<<"Enter a string: ";
+    getline(cin, str); // This will take the whole line as input including spaces
+    cout << "the input is: " << str << endl; // This will print the whole line including spaces
+
+    return 0;
+}
+
+// Enter a string: hi myself allen
+// the input is: hi myself allen
+// PS D:\code\Leet-Code> 
