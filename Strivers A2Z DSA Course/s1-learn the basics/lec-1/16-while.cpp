3d9922259eec9bc9c2ec93a506d58f37f6266ed7
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-18 22:49:18 +0530

    while loop

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/16-while.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int i = 0;
+
+    while(i<=5){
+        cout << "count" << i << endl;
+
+        i++;
+    }
+
+
+}
+
+
+// count0
+// count1
+// count2
+// count3
+// count4
+// count5
+// PS D:\code\Leet-Code> 
