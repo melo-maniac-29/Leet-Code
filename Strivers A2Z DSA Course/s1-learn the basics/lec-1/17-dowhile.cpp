541fbc64ec34d1621fd30f3828c4012955b1a0c4
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-18 22:53:41 +0530

    do-while

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/17-dowhile.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int i=1;
+
+    do{
+        cout << "count" << i << endl;
+
+        i++;
+    }while(i<=5);
+
+    cout << "End of do while loop" << endl;
+    cout << "i = " << i << endl;
+
+}
+
+
+// count1
+// count2
+// count3
+// count4
+// count5
+// End of do while loop
+// i = 6
