ab9aee57e1f04fe34a829a7b967b53d3dffbf1c3
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-18 20:47:24 +0530

    for-loop

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/15-for.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    for(int i = 0; i < 5; i++){
+        cout << "Hello World" << endl;
+    }
+
+    for(int j=1;j<=10;j++){
+        cout << j << " ";
+    }
+
+    return 0;
+}
+
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// 1 2 3 4 5 6 7 8 9 10
+// PS D:\code\Leet-Code> 
