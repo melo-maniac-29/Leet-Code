7ffc35607835895d9258dc5fba0ee05421433d33
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-18 20:35:16 +0530

    2D array

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-1/13-2D-array.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // 2D array declaration
+
+    int arr[3][4]; // 3 rows and 4 columns
+    // 2D array initialization
+
+    arr[0][0] = 1;
+    arr[0][1] = 2;
+    arr[0][2] = 3;
+    arr[0][3] = 4;
+
+    cout << arr[0][0] << " " << arr[0][1] << " " << arr[0][2] << " " << arr[0][3] << endl;
+
+    return 0;
+}
