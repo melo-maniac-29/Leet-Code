5c9e5d25aead42e02403adc9956028b43874a68d
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-20 23:41:02 +0530

    p-6

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-2/7-pattern6.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout<< "* " ;
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    print(n);
+}
+
+
+
+// Enter the number of rows: 5
+// * * * * * 
+// * * * *
+// * * *
+// * *
+// *
+// PS D:\code\Leet-Code> 
