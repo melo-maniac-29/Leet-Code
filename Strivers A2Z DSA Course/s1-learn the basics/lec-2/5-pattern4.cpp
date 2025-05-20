eb4897987abc1e89697e748c3167a4d560f078ce
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-20 23:11:11 +0530

    p4

--- Strivers A2Z DSA Course/s1-learn the basics/lec-2/5-pattern4.cpp
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-2/5-pattern4.cpp
@@ -4,10 +4,27 @@ using namespace std;
 void print(int n){
     for(int i=1;i<=n;i++)
     {
-        for(int j=0;j<i;j++)
+        for(int j=1;j<=i;j++)
         {
-            cout<< i << " " ;
+            cout<< j << " " ;
         }
         cout<<endl;
     }
 }
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    print(n);
+    return 0;
+}
+
+
+// Enter the number of rows: 4
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4
+// PS D:\code\Leet-Code> 
