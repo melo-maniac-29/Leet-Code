5101d4c162b8dfef08f18a2e50839628e496cfd3
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-20 23:08:28 +0530

    p3

--- Strivers A2Z DSA Course/s1-learn the basics/lec-2/4-pattern3.cpp
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-2/4-pattern3.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void print(int n){
-    for(int i=0;i<=n;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<i;j++)
+        for(int j=0;j<=i;j++)
         {
             cout<< "* " ;
         }
---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-2/5-pattern4.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<< i << " " ;
+        }
+        cout<<endl;
+    }
+}
