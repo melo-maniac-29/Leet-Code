ba8c0bbeaffca4ca2541ed4ad5acce018853b1f4
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-20 22:53:29 +0530

    p1

---
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-2/2-pattern1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cout<< "*" ;
+        }
+        cout<<endl;
+    }
+
+
+}
+
+
+// *****
+// *****
+// *****
+// *****
+// *****
+// PS D:\code\Leet-Code> 
