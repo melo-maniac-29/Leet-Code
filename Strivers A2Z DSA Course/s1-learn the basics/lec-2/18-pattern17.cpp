7eb306b8775523de169d51774bd90cce14e85974
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-21 22:29:07 +0530

    p-17

--- Strivers A2Z DSA Course/s1-learn the basics/lec-2/18-pattern17.cpp
+++ Strivers A2Z DSA Course/s1-learn the basics/lec-2/18-pattern17.cpp
@@ -11,4 +11,31 @@ using namespace std;
 
 void print(int n){
     
+    char ch='A';
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout << ch << " ";
+        }    
+        cout << endl;
+        ch++;
+    }
 }
+
+int main(){
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    print(n);
+    return 0;
+}
+
+
+
+// Enter the number of rows: 5
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E 
+// PS D:\code\Leet-Code> 
